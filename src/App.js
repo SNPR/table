1adf46d4aeaeb6b30c4cf36af30f00fbf7e1f6ea
snpr <vaugk13@gmail.com>
2018-12-11 22:38:18 +0500

    Первая версия таблицы с данными.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-class App extends Component {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { data: null };
+  }
+  componentDidMount() {
+    let request = new XMLHttpRequest();
+    request.open("GET", "http://www.filltext.com/?rows=10&fname={firstName}&lname={lastName}&tel={phone|format}&city={city}", true);
+    request.onreadystatechange = () => {
+      if (request.readyState !== 4 || request.status !== 200) return;
+      var data = JSON.parse(request.responseText);
+      this.setState({ data });
+    };
+    request.send();
+  }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    let component = null;
+    if (!this.state.data) {
+      component = <h1>Loading...</h1>;
+    } else {
+      component = (
+        <table className="test-task-table">
+        <caption>Таблица с тестовыми данными, подтянутыми с filltext.com</caption>
+          <tr>
+            <th>Имя</th>
+            <th>Фамилия</th>
+            <th>Телефон</th>
+            <th>Город</th>
+          </tr>
+          {this.state.data.map((entry, i) => (
+            <tr>
+              <td key={i}>{entry.fname}</td>
+              <td key={i}>{entry.lname}</td>
+              <td key={i}>{entry.tel}</td>
+              <td key={i}>{entry.city}</td>
+            </tr>
+          ))}
+        </table>
+      );
+    }
+    return component;
   }
 }
 
